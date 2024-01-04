171c7bc9c46f6b7220c8c1de15c8f2f6891ba026
Aleksandr Prohorets <a.prakharets@setplex.com>
2024-01-04 16:49:15 +0300

    feat: added NoraAPISubscriber types export

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,3 @@
 export { NoraAPI } from './resource';
 export { PaymentSystemTypes as NoraAPIPaymentSystemTypes, ExternalPaymentSystemTypes as NoraAPIExternalPaymentSystemTypes, AddOnType as NoraAPIAddOnType, SubscriberPaymentStatuses as NoraAPISubscriberPaymentStatuses, PaymentSystems as NoraAPIPaymentSystems, } from './resource/types/constants';
+export { NoraAPISubscriber } from './resource/types/subscribers.types';
