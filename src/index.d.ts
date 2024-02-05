05a43779fa4c1880cd262a86c4c0a9854fc3a867
Ivan Morozov <ivanm@setplex.com>
2024-02-05 14:00:13 +0300

    Export subscription type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,4 @@
 export { NoraAPI } from './resource';
 export { PaymentSystemTypes as NoraAPIPaymentSystemTypes, ExternalPaymentSystemTypes as NoraAPIExternalPaymentSystemTypes, AddOnType as NoraAPIAddOnType, SubscriberPaymentStatuses as NoraAPISubscriberPaymentStatuses, PaymentSystems as NoraAPIPaymentSystems, } from './resource/types/constants';
 export { NoraAPISubscriber, NoraApiSubscriberSubscription, NoraApiSubscriberSubscriptionContentSets, } from './resource/types/subscribers.types';
+export { NoraAPISubscription } from './resource/types/subscriptions.types';
