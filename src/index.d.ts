fad53e66c89788a21fd3a8490d7e8f8e463c4970
Ivan Morozov <ivanm@setplex.com>
2024-02-14 17:45:28 +0300

    fix: export NoraAPIPaymentSystemsOptionsRequest

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,3 +2,4 @@ export { NoraAPI } from './resource';
 export { PaymentSystemTypes as NoraAPIPaymentSystemTypes, ExternalPaymentSystemTypes as NoraAPIExternalPaymentSystemTypes, AddOnType as NoraAPIAddOnType, SubscriberPaymentStatuses as NoraAPISubscriberPaymentStatuses, PaymentSystems as NoraAPIPaymentSystems, AccessoryTypes as NoraAPIAccessoryTypes } from './resource/types/constants';
 export { NoraAPISubscriber, NoraApiSubscriberSubscription, NoraApiSubscriberSubscriptionContentSets, } from './resource/types/subscribers.types';
 export { NoraAPISubscription } from './resource/types/subscriptions.types';
+export { NoraAPIPaymentSystemsOptionsRequest } from './resource/types/payment-systems.types';
