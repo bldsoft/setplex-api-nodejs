f6b892b4fee8ddbcd7d702347d4a744bd62e9f87
Aleksandr Prohorets <a.prakharets@setplex.com>
2024-06-10 14:20:22 +0300

    fix: GetContentRent response type

--- src/resource/types/content-rent.types.d.ts
+++ src/resource/types/content-rent.types.d.ts
@@ -1,4 +1,4 @@
-import type { CommonRequest, CommonResponse, OperationStatusResponse } from './index';
+import type { CommonRequest, CommonResponse } from './index';
 import { NoraAPISubscriberContentRentProlongation } from './subscribers.types';
 /** Interface of the request object for call /content-rent-prolongations/get */
 export interface NoraAPIGetContentRentRequest extends CommonRequest {
@@ -10,17 +10,18 @@ export interface NoraAPIGetContentRentRequest extends CommonRequest {
     size: number;
 }
 /** Interface of the response object for call /content-rent-prolongations/get */
-export interface NoraAPIGetContentRentResponse {
-    content: NoraAPISubscriberContentRentProlongation[];
-    /** Number of the displayed page. */
-    number: number;
-    /** Maximum elements, which are displayed on the page. */
-    size: number;
-    /** The flag which describes if the page is first. */
-    first: boolean;
-    /** The flag which describes if the page is last. */
-    last: boolean;
-    status: OperationStatusResponse;
+export interface NoraAPIGetContentRentResponse extends CommonResponse {
+    result: {
+        content: NoraAPISubscriberContentRentProlongation[];
+        /** Number of the displayed page. */
+        number: number;
+        /** Maximum elements, which are displayed on the page. */
+        size: number;
+        /** The flag which describes if the page is first. */
+        first: boolean;
+        /** The flag which describes if the page is last. */
+        last: boolean;
+    };
 }
 /** Interface of the request object for call /content-rent-prolongations/autopay */
 export interface NoraAPIAutopayContentRentRequest extends CommonRequest {
