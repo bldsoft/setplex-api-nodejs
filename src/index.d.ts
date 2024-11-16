96dc4929ac905b833cca08e709c5a6a4004ef127
Ivan Morozov <ivanm@setplex.com>
2024-11-16 10:41:55 +0300

    fix: export to 2c2p payment result statuses

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,6 @@
 export { NoraAPI } from './resource';
 export { NoraApiError } from './errors/nora-api.error';
-export { PaymentSystemTypes as NoraAPIPaymentSystemTypes, ExternalPaymentSystemTypes as NoraAPIExternalPaymentSystemTypes, AddOnType as NoraAPIAddOnType, SubscriberPaymentStatuses as NoraAPISubscriberPaymentStatuses, PaymentSystemTypes as NoraAPIPaymentSystems, AccessoryTypes as NoraAPIAccessoryTypes, } from './resource/types/constants';
+export { PaymentSystemTypes as NoraAPIPaymentSystemTypes, ExternalPaymentSystemTypes as NoraAPIExternalPaymentSystemTypes, AddOnType as NoraAPIAddOnType, SubscriberPaymentStatuses as NoraAPISubscriberPaymentStatuses, PaymentSystemTypes as NoraAPIPaymentSystems, AccessoryTypes as NoraAPIAccessoryTypes, PaymentSystem2C2PStatusOfThePurchase as PaymentSystem2C2PPurchaseStatuses, } from './resource/types/constants';
 export { NoraAPISubscriber, NoraApiSubscriberSubscription, NoraApiSubscriberSubscriptionContentSets, NoraApiSubscriberSubscriptionPartnerProduct, NoraAPISubscriberContentRentProlongation, } from './resource/types/subscribers.types';
 export { NoraAPISubscription, NoraAPISubscriptionsPlan } from './resource/types/subscriptions.types';
 export { NoraAPIPaymentSystemsOptionsRequest } from './resource/types/payment-systems.types';
